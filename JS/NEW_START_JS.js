739e500ce9fa53e301a1acdc98688406df6f0782
APSODE <81503599+APSODE@users.noreply.github.com>
2021-09-10 01:13:28 +0900

    JavaScript Study START!!!!!

--- JS/NEW_START_JS.js
+++ JS/NEW_START_JS.js
@@ -1,1 +1,8 @@
-alert("HI");
+const PTAG = document.querySelector("p") 
+
+PTAG.addEventListener("click", UPDATE_NAME) 
+
+function UPDATE_NAME() {
+    let NAME = prompt("ENTER A NEW NAME!")
+    PTAG.textContent = "Player 1: " + NAME
+}
